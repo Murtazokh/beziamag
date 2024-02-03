3bf89904ec6ed0b95966a8be7fff9e7955360256
murtazokhon <murtazoxon12@gmail.com>
2024-02-03 17:29:35 +0500

    add search functionality

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -10,65 +10,117 @@ function DeactivateTabItems () {
   });
 }
 
-function DeactivateTabpanels () {
-  elsTabpanels.forEach(function (elTabpanel) {
-    elTabpanel.classList.remove('tabpanels__item--active');
-  });
-}
-
-
 elsTabLink.forEach(function (elTabLink) {
   elTabLink.addEventListener('click', function (evt) {
     evt.preventDefault();
     DeactivateTabItems();
     elTabLink.parentElement.classList.add('tabs__item--active');
-    DeactivateTabpanels();
-    // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
-    const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
-    elTargetPanel.classList.add('tabpanels__item--active');
   });
 });
 
+
+function showTab(tabNumber) {
+  // Barcha panellar yashiriladi
+  const tabPanels = document.querySelectorAll('.tab-panel');
+  tabPanels.forEach(panel => {
+    panel.style.display = 'none';
+    panel.style.opacity = 0;
+  });
+
+  // Tanlangan tabni ko'rsatish
+  const selectedTab = document.getElementById(`tab${tabNumber}`);
+  selectedTab.style.display = 'block';
+
+  // Tranzitsiyalarni boshlash
+  setTimeout(() => {
+    selectedTab.style.opacity = 1;
+  }, 10);
+}
+
 // =================
 
-document.querySelector('.btn--right').addEventListener('click', function() {
-  const productList = document.querySelector('.products__list');
-  // Boshqa talablar bo'yicha shu erda scroll qilishingiz mumkin
-  productList.scrollLeft += 100; // Masalan, har safar 100 piksel o'nga o'chadi
-});
+// Sticky header
+window.onscroll = function() {stickyHeader()};
 
-document.querySelector('.btn--left').addEventListener('click', function() {
-  const productList = document.querySelector('.products__list');
-  // Boshqa talablar bo'yicha shu erda scroll qilishingiz mumkin
-  productList.scrollLeft -= 100; // Masalan, har safar 100 piksel o'nga o'chadi
-});
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
+const hero = document.querySelector('.hero');
+const contactPage = document.querySelector('.contact-page');
+const aboutPage = document.querySelector('.about-page');
+const productsInner = document.querySelector('.products__inner');
 
 
-let intervalId;
-
-function startMoving(direction) {
-  const productList = document.querySelector('.products__list');
-  const speed = 2; // Harakatlanish tezligi (px)
-
-  intervalId = setInterval(function() {
-    if (direction === 'right') {
-      productList.scrollLeft += speed;
-    } else if (direction === 'left') {
-      productList.scrollLeft -= speed;
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    if (hero) {
+      hero.classList.add('hero-pt');
+    };
+    if (contactPage) {
+      contactPage.style.paddingTop = '240px';
+    };
+    if (aboutPage) {
+      aboutPage.style.paddingTop = '240px';
+    };
+    if (productsInner) {
+      productsInner.style.paddingTop = '190px';
     }
-  }, 10); // 10 millisekundda bir harakatlanish
+  } else {
+    header.classList.remove("sticky");
+    if (hero) {
+      hero.classList.remove('hero-pt');
+    }
+    if (contactPage) {
+      contactPage.style.paddingTop = '';
+    }
+    if (aboutPage) {
+      aboutPage.style.paddingTop = '';
+    }
+    if (productsInner) {
+      productsInner.style.paddingTop = '';
+    }
+  }
 }
 
-function stopMoving() {
-  clearInterval(intervalId);
-}
+// // == Product cards
+// function productCard(product) {
+//   return `<li class="products__list-item product-card">
+//   <a> <img class="product-card__img" src="${product.thumbnail}" alt="${product.title}">
+//   <div class="product-card__info">
+//     <h3 class="product-card__title">${product.title}</h3>
+//     <span class="product-card__price">$${product.price}</span>
+//   </div></a>
+//   </li>`;
+// }
 
-document.querySelector('.btn--left').addEventListener('mousedown', function() {
-  startMoving('left');
-});
+// async function appendCard(skip, count) {
+//   const elProductList = document.querySelector('.products__list');
+//   let cardsHtml = '';
 
-document.querySelector('.btn--right').addEventListener('mousedown', function() {
-  startMoving('right');
-});
+//   if (elProductList) {
+//     try {
+//       // API dan ma'lumotlarni olish
+//       const res = await fetch("https://dummyjson.com/products");
+//       const jsonData = await res.json();
 
-document.addEventListener('mouseup', stopMoving);
+//       // Agar ma'lumotlar "products" nomli maydon ichida bo'lsa
+//       const data = jsonData.products;
+
+//       // Faqat count ta ma'lumotni olish
+//       const limitedData = data.slice(skip, count);
+
+//       // Har bir mahsulot uchun cardHtml ni qo'shish
+//       limitedData.forEach((product) => {
+//         cardsHtml += productCard(product);
+//       });
+
+//       // ProductList ga cardHtml ni qo'shish
+//       elProductList.innerHTML += cardsHtml;
+//     } catch (error) {
+//       console.error("Ma'lumotlarni olishda xatolik:", error);
+//     }
+//   }
+// }
+
+// appendCard funksiyasini chaqirish
+// appendCard(8);
